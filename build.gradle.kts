fb35003bb2c0f207ac8db8982bf638530a1fb69b
mrmpnm2015-alt <mrm.pnm.2015@gmail.com>
2026-08-01 07:53:57 +0330

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// این فایل در صفحه اصلی پروژه قرار دارد (Root)
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+}
